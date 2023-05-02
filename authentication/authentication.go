e4e4842d7696b6b00ae2761bc209c62412cd924f
Randy Deborggraeve <randy@deb-ict.com>
2023-05-02 21:13:59 +0200

    update authentication context
    Signed-off-by: Randy Deborggraeve <randy@deb-ict.com>

--- authentication/authentication.go
+++ authentication/authentication.go
@@ -2,6 +2,7 @@ package authentication
 
 import (
 	"context"
+	"strings"
 
 	"github.com/deb-ict/go-router"
 )
@@ -18,17 +19,81 @@ const (
 	authenticationContextKey router.ContextKey = "router::authentication"
 )
 
-type AuthenticationContext struct {
+type Claim struct {
+	Name   string
+	Values []string
 }
 
-func GetAuthenticationContext(ctx context.Context) *AuthenticationContext {
+type ClaimMap map[string]Claim
+
+type AuthenticationContext interface {
+	IsAuthenticated() bool
+	GetClaimMap() ClaimMap
+	GetClaim(name string) Claim
+	GetClaimValue(name string, index int) string
+	HasClaim(name string) bool
+	HasClaimValue(name string, value string) bool
+}
+
+type authenticationContext struct {
+	authenticated bool
+	claims        map[string]Claim
+}
+
+func GetAuthenticationContext(ctx context.Context) AuthenticationContext {
 	value := ctx.Value(authenticationContextKey)
 	if value == nil {
 		return nil
 	}
-	return value.(*AuthenticationContext)
+	return value.(AuthenticationContext)
 }
 
-func SetAuthenticationContext(ctx context.Context, auth *AuthenticationContext) context.Context {
+func SetAuthenticationContext(ctx context.Context, auth AuthenticationContext) context.Context {
 	return context.WithValue(ctx, authenticationContextKey, auth)
+}
+
+func (ctx *authenticationContext) IsAuthenticated() bool {
+	return ctx.authenticated
+}
+
+func (ctx *authenticationContext) GetClaimMap() ClaimMap {
+	return ctx.claims
+}
+
+func (ctx *authenticationContext) GetClaim(name string) Claim {
+	return ctx.claims[name]
+}
+
+func (ctx *authenticationContext) GetClaimValue(name string, index int) string {
+	c := ctx.GetClaim(name)
+	if len(c.Values) <= index {
+		return ""
+	}
+	return c.Values[index]
+}
+
+func (ctx *authenticationContext) HasClaim(name string) bool {
+	_, ok := ctx.claims[name]
+	return ok
+}
+
+func (ctx *authenticationContext) HasClaimValue(name string, value string) bool {
+	c := ctx.GetClaim(name)
+	return c.HasValue(value)
+}
+
+func (c *Claim) First() string {
+	if len(c.Values) == 0 {
+		return ""
+	}
+	return c.Values[0]
+}
+
+func (c *Claim) HasValue(value string) bool {
+	for _, v := range c.Values {
+		if strings.EqualFold(v, value) {
+			return true
+		}
+	}
+	return false
 }
